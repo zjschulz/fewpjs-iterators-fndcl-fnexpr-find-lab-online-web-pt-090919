9cfe0a9ceaf86769d808f6a4a48c210dc135739d
zjschulz <zjschulz@gmail.com>
2020-04-15 18:18:35 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  const win = record.find( ({ result }) => result === 'W' );
+  if (win) {
+    return win.year;
+  }
+}
+
+// s = {year: "2018", result: "L"}
